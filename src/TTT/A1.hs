9dcf91714673994bc895d0292719b314a2fc6468
Ryan Sargent <sargent11235@gmail.com>
2023-10-14 14:42:15 +0000

    halfway through homework 2

--- src/TTT/A1.hs
+++ src/TTT/A1.hs
@@ -4,7 +4,7 @@ import Data.Char (toUpper)
 
 -- Q#01
 _SIZE_ :: Int
-_SIZE_ = 5
+_SIZE_ = 3
 
 -- Q#02
 _DISPLAY_LOGO_ :: Bool
--- src/TTT/A2.hs
+++ src/TTT/A2.hs
@@ -2,26 +2,39 @@ module TTT.A2 where
 
 import Data.List (intercalate)
 import TTT.A1
+import Data.String (IsString)
+
 
 -- Q#01
+promptPlayer :: Player -> String
+promptPlayer p = concat["Player ", showSquare p, "'s turn: enter a row and column position (ex. A1)"] 
 
-promptPlayer = undefined
 
 -- Q#02
 
-_RANGE_ = undefined
+_RANGE_ :: [Int]
+_RANGE_ = [0.. _SIZE_ -1]
 
 -- Q#03
 
-isDigit = undefined
+isDigit :: Char -> Bool
+isDigit c = 
+    let charList = ['0' .. '9']
+    in c `elem` charList 
 
-readDigit = undefined
+readDigit :: Char -> Int
+readDigit c 
+    | isDigit c = read [c] 
+    | otherwise = -1
+    
 
 -- Q#04
 
-_EMPTY_ROW_ = undefined
+_EMPTY_ROW_ :: [Square]
+_EMPTY_ROW_ = replicate _SIZE_ Neither 
 
-_EMPTY_BOARD_ = undefined
+_EMPTY_BOARD_ :: [[Square]]
+_EMPTY_BOARD_ = replicate _SIZE_ _EMPTY_ROW_
 
 -- Q#05
 
