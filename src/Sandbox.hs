6dd761f51abbf20e9bb87a7b1b406f47ae0e7097
Ryan Sargent <sargent11235@gmail.com>
2023-10-19 12:48:36 +0000

    added sandbox notes

--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -1,2 +1,35 @@
 module Sandbox where
+import Debug.Trace
+
+
+-- recursion examples
+factorial :: Int -> Int
+factorial 0 = 1 
+factorial 1 = 1
+factorial n 
+    | n > 1 = n * factorial (n-1)
+    | otherwise = error "Negetive number"
+
+length' :: [a] -> Int
+length' [] = 0
+length' (x:xs) = 1 + length' (xs)
+
+sum' :: [Int] -> Int
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+-- debug fucntion trace for the sum' function
+--sum' (x:xs) = trace ("sum' " ++ show xs) (x + sum' xs)
+
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x] 
+
+
+--iterative recursion approach 
+sum_iter :: [Int] -> Int
+sum_iter xs = loop 0 xs
+    where 
+        loop count [] = count
+        loop count (h:t) = loop (count + h) t 
 
