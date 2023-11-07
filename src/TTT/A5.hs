b1c2b310b123d00ec732c1073cf04a6d15bf56b4
Ryan Sargent <sargent11235@gmail.com>
2023-11-07 17:28:24 +0000

    completed hw5

--- src/TTT/A5.hs
+++ src/TTT/A5.hs
@@ -18,9 +18,7 @@ _LOGO_PATH_ :: FilePath
 _LOGO_PATH_ = "./assets/ttt-logo.txt"
 
 printLogo :: IO () 
-printLogo = do 
-    contents <- readFile _LOGO_PATH_ 
-    putStrLn contents  
+printLogo = readFile _LOGO_PATH_ >>= putStrLn   
 
 
 -- Q#03
@@ -28,15 +26,60 @@ _RANDOM_BOOL_ :: IO Bool
 _RANDOM_BOOL_ = uniformM globalStdGen
 
 firstPlayer :: IO Player   
-firstPlayer = do 
-    result <- _RANDOM_BOOL_
-    return (getFirstPlayer result) 
-
+firstPlayer = _RANDOM_BOOL_ >>= \result -> return (getFirstPlayer result)
 
 -- Q#04
 
 getMove :: Board -> IO Move  
-getMove b  = do 
+getMove b  = 
+    putStrLn "Enter a move" >>
+    getLine >>= \input ->
+    let move = stringToMove input 
+    in if isValidMove b move then
+            return move 
+        else 
+            putStrLn "Invalid move bitch. Try again" >>
+            getMove b     
+
+-- Q#05
+
+play :: Board -> Player -> IO ()
+play b p = printLogo >> printBoard b >> 
+            putStrLn (promptPlayer p) >> 
+            getMove b >>= \move -> 
+            let (state, newBoard) = playMove p b move 
+            in case state of 
+                InProgress -> play newBoard p 
+                XWon -> putStrLn "X won bitch"
+                OWon -> putStrLn "O won bitch"
+                Tie -> putStrLn "It's a tie bitch"
+      
+-- Q#06
+
+runTTT :: IO ()
+runTTT = do  
+    putStrLn "Not implemented... yet!"
+    playDo _EMPTY_BOARD_ =<< firstPlayer 
+
+
+-- Q#07
+
+printLogoDo :: IO () 
+printLogoDo = do 
+    contents <- readFile _LOGO_PATH_ 
+    putStrLn contents 
+
+-- Q#08
+
+firstPlayerDo :: IO Player   
+firstPlayerDo = do 
+    result <- _RANDOM_BOOL_
+    return (getFirstPlayer result) 
+
+-- Q#09
+
+getMoveDo :: Board -> IO Move  
+getMoveDo b  = do 
     putStrLn "Enter a move"
     input <- getLine 
     let move = stringToMove input 
@@ -45,43 +88,18 @@ getMove b  = do
     else do 
         putStrLn "Invalid move bitch. Try again"
         getMove b 
-            
-
--- Q#05
-
-play :: Board -> Player -> IO ()
-play b p = do 
-    printLogo
-    printBoard b 
-    putStrLn $ promptPlayer p
-    move <- getMove b 
-    let (state, newBoard) = playMove p b move 
-    case state of 
-        InProgress -> play newBoard p 
-        XWon -> putStrLn "X won bitch"
-        OWon -> putStrLn "O won bitch"
-        Tie -> putStrLn "It's a tie bitch"
-      
--- Q#06
-
-runTTT :: IO ()
-runTTT = do  
-    putStrLn "Not implemented... yet!"
-    play _EMPTY_BOARD_ =<< firstPlayer 
-
-
--- Q#07
-
-printLogoDo = undefined
-
--- Q#08
-
-firstPlayerDo = undefined
-
--- Q#09
-
-getMoveDo = undefined
 
 -- Q#10
 
-playDo = undefined
+playDo :: Board -> Player -> IO ()
+playDo b p = do 
+    printLogoDo
+    printBoard b 
+    putStrLn $ promptPlayer p
+    move <- getMoveDo b 
+    let (state, newBoard) = playMove p b move 
+    case state of 
+        InProgress -> playDo newBoard p 
+        XWon -> putStrLn "X won bitch"
+        OWon -> putStrLn "O won bitch"
+        Tie -> putStrLn "It's a tie bitch"
