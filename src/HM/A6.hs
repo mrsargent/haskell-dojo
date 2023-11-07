99d34f85da53176d3e1a6d2b24ec73dbea219aba
Ryan Sargent <sargent11235@gmail.com>
2023-11-07 22:22:56 +0000

    working on HW6

--- src/HM/A6.hs
+++ src/HM/A6.hs
@@ -2,23 +2,62 @@ module HM.A6 where
 
 import Data.Char (isAlpha)
 import HM.Provided
+import System.IO
 
 -- Q#01
+type Chances = Int
+type Guess = String 
+type Move = Char 
+type Secret = String 
+type Dictionary = [String] 
 
 -- Q#02
-data GameException
+data GameException = InvalidChars | InvalidLength | NotInDict | InvalidMove | RepeatMove | GameOver 
 
 -- Q#03
 
-lengthInRange = undefined
+lengthInRange :: Secret -> Bool 
+lengthInRange s =
+    let (min,max) = _LENGTH_
+        secretLength = length s  
+    in secretLength <= max && secretLength >= min
+   
 
 -- Q#04
-
-invalidMove = undefined
+invalidMove :: Move -> Bool 
+invalidMove c 
+    | isAlpha c     = True
+    | otherwise     = False 
 
 -- Q#05
+{- revealLetters :: Move -> Secret -> Guess -> Guess 
+revealLetters m s g = do 
+    let len = length s 
+        str = replicate len '_'
+    putStrLn $ str 
+    putStrLn "Take a guess"
+    gue <- getLine -}
+    
+    -- Function to reveal letters in the guess
+revealLetters :: Move -> Secret -> Guess -> Guess
+revealLetters move secret guess =
+    let revealFunc x y = if y == move then y else x
+    in zipWith revealFunc guess secret
 
-revealLetters = undefined
+-- Example usage
+main :: IO ()
+main = do
+    let secret = "Haskell"
+        guess = replicate (length secret) '_'
+        move = 'l'
+        updatedGuess = revealLetters move secret guess
+    putStrLn $ "Secret: " ++ secret
+    putStrLn $ "Initial Guess: " ++ guess
+    putStrLn $ "Move: " ++ [move]
+    putStrLn $ "Updated Guess: " ++ updatedGuess
+
+
+    
 
 -- Q#06
 
