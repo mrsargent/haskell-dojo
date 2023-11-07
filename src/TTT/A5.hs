6e7b9ac4b5fb0a502264a74db9437df3483514ae
Ryan Sargent <sargent11235@gmail.com>
2023-11-07 00:51:29 +0000

    in progress on HW5

--- src/TTT/A5.hs
+++ src/TTT/A5.hs
@@ -6,30 +6,64 @@ import TTT.A1
 import TTT.A2
 import TTT.A3
 import TTT.A4
+import System.IO
+import GHC.Read (readField)
 
 -- Q#01
-
-printBoard = undefined
+printBoard :: Board -> IO () 
+printBoard b = putStrLn (formatBoard b) 
 
 -- Q#02
 _LOGO_PATH_ :: FilePath
 _LOGO_PATH_ = "./assets/ttt-logo.txt"
 
-printLogo = undefined
+printLogo :: IO () 
+printLogo = do 
+    contents <- readFile _LOGO_PATH_ 
+    putStrLn contents  
+
 
 -- Q#03
 _RANDOM_BOOL_ :: IO Bool
 _RANDOM_BOOL_ = uniformM globalStdGen
 
-firstPlayer = undefined
+firstPlayer :: IO Player   
+firstPlayer = do 
+    result <- _RANDOM_BOOL_
+    return (getFirstPlayer result) 
+
 
 -- Q#04
 
-getMove = undefined
+getMove :: Board -> IO Move  
+getMove b  = do 
+    putStrLn "Enter a move"
+    input <- getLine 
+    let move = stringToMove input 
+    if isValidMove b move then
+        return move 
+    else do 
+        putStrLn "Invalid move bitch. Try again"
+        getMove b 
+            
 
 -- Q#05
 
-play = undefined
+play :: Board -> Player -> IO ()
+play b p = do 
+    printLogo
+    printBoard b 
+    putStrLn $ promptPlayer p
+    move <- getMove b 
+    let (state, newBoard) = playMove p b move 
+    case state of 
+        InProgress -> play newBoard p 
+        XWon -> putStrLn "X won bitch"
+        OWon -> putStrLn "O won bitch"
+        Tie -> putStrLn "It's a tie bitch"
+      
+
+
 
 -- Q#06
 
