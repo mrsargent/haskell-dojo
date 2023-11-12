d2b517d0d574c0d2f540406b22adb9715a1eba1d
Ryan Sargent <sargent11235@gmail.com>
2023-11-12 23:57:35 +0000

    finish hm6

--- src/HM/A6.hs
+++ src/HM/A6.hs
@@ -30,49 +30,55 @@ invalidMove c
     | otherwise     = False 
 
 -- Q#05
-{- revealLetters :: Move -> Secret -> Guess -> Guess 
-revealLetters m s g = do 
-    let len = length s 
-        str = replicate len '_'
-    putStrLn $ str 
-    putStrLn "Take a guess"
-    gue <- getLine -}
-    
     -- Function to reveal letters in the guess
 revealLetters :: Move -> Secret -> Guess -> Guess
 revealLetters move secret guess =
     let revealFunc x y = if y == move then y else x
     in zipWith revealFunc guess secret
 
--- Example usage
-main :: Move -> Secret -> Guess -> IO ()
-main move secret guess = do
-    putStr "Enter a letter you think is in the secret word: "
-    m <- getLine
-    let move = head m 
-        s = "bitch"
-        updatedGuess = zipWith (\x y -> if y == move then y else x) s guess    
-    putStrLn $ "Secret: " ++ secret
-    putStrLn $ "Initial Guess: " ++ guess
-    putStrLn $ "Move: " ++ [move]
-    putStrLn $ "Updated Guess: " ++ updatedGuess
-    if '_' `elem` updatedGuess then 
-        putStrLn "keep guessing bitch"
-        main '_' s updatedGuess 
+playGame :: Secret -> Guess -> IO () 
+playGame secret guess = do 
+    putStrLn $ "Here is your current guesses: " ++ guess 
+    putStr "Enter a character you think is in the secret phrase: "
+    move <- getLine
+
+    let m = head move
+        updatedGuess = revealLetters m secret guess 
+
+    if updatedGuess == secret then
+        putStrLn $ "Congrats you won. You guessed the secret word: " ++ secret
     else
-        return updatedGuess
+        playGame secret updatedGuess 
 
 
-
-
-
-    
+-- Example usage
+main :: IO ()
+main = do
+    let secret = "haskell"
+        iniitialGuess = replicate (length secret) '_'
+    putStrLn "Let's play a guessing game"
+    playGame secret iniitialGuess    
 
 -- Q#06
-
-updateChances = undefined
+updateChances :: Move -> Secret -> Chances -> Chances 
+updateChances move secret chance = 
+    if move `elem` secret then
+        chance 
+    else
+        chance - 1 
 
 -- Q#07
+setSecret :: IO ()
+setSecret = do 
+    putStr "Enter a secret phrase you fucking pussy:  "
+    showInput True 
+    secret <- getLine 
+    putStrLn $ "You entered the word " ++ secret
+    _SPACE_
+    putStr "Enter another secret phrase you fucking pussy:  "
+    showInput False
+    secret <- getLine    
+    putStrLn $ "You entered the word " ++ secret
 
-setSecret = undefined
+
 
